aed515d25c24bca540fe5daaeb603e30439766de
Stephen Drodge <stephen.drodge@gmail.com>
2014-10-06 13:29:41 -0400

    Minor changes to git prompt.
    Removed settings that no longer exist in __fish_git_prompt. Changed the
    order of settings to match the order in which the documentation presents
    them. Enabled "branch-style" descriptions for detached states.

--- config.fish
+++ config.fish
@@ -14,22 +14,32 @@ source $HOME/.config/fish/virtualfish/auto_activation.fish
 
 # Git prompt variables
 set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
+# set -g __fish_git_prompt_showupstream 'informative'
+set -g __fish_git_prompt_showstashstate 1
+set -g __fish_git_prompt_describe_style 'branch'
 
-set -g __fish_git_prompt_color_branch magenta --bold
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
+set -g __fish_git_prompt_char_upstream_prefix ''
+set -g __fish_git_prompt_char_upstream_ahead '↑'
+set -g __fish_git_prompt_char_upstream_behind '↓'
+# set -g __fish_git_prompt_char_stateseparator '❘'
+set -g __fish_git_prompt_char_dirtystate '✚'
+set -g __fish_git_prompt_char_invalidstate '✖'
+set -g __fish_git_prompt_char_stagedstate '●'
+set -g __fish_git_prompt_char_untrackedfiles '…'
+set -g __fish_git_prompt_char_cleanstate '✔'
+set -g __fish_git_prompt_char_stashstate '$'
 
 set -g __fish_git_prompt_color_dirtystate cyan
-set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate $fish_color_error
+set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
 set -g __fish_git_prompt_color_cleanstate green --bold
+# set -g __fish_git_prompt_color_stashstate
+
+# set -g __fish_git_prompt_color_prefix
+# set -g __fish_git_prompt_color_suffix
+# set -g __fish_git_prompt_color_bare
+# set -g __fish_git_prompt_color_merging
+set -g __fish_git_prompt_color_branch magenta --bold
+# set -g __fish_git_prompt_color_flags
+# set -g __fish_git_prompt_color_upstream
